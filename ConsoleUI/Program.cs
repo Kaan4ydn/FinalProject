ef934653807d2afb59c049d313a9057f8ee09e9e
kaan aydin <aydinkaan2534@gmail.com>
2022-11-16 09:57:05 +0300

    Yazılım Geliştirici Yetiştirme Kampı 12. Gün C# Kurumsal Yazılım Mimarileri Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
 
             //CategoryTest();
         }
@@ -32,9 +32,18 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.IsSuccess == true)
             {
-                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
